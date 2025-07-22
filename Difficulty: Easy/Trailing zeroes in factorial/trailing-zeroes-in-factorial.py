f551c6e585f2bc7f77275de54ef5ba0b72e398f3
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2025-07-22 20:40:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Trailing zeroes in factorial/trailing-zeroes-in-factorial.py
@@ -0,0 +1,11 @@
+import math
+class Solution:
+    def trailingZeroes(self, n):
+    	#code here 
+    	if n<0:
+    	    return -1
+    	count = 0
+    	while(n>=5):
+    	    n //= 5
+    	    count += n
+    	return count
